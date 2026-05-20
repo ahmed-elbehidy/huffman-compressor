7388a9b3127502f0dc7b7446e2505a1d8c41e02a
Ahmed Behidy <ahmedelbehidy2004@gmail.com>
2026-05-21 01:27:28 +0300

    Add project files.

---
+++ file_compression/src/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <queue>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    
+
+    return 0;
+}
